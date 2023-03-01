92d0fb89e02cef7f7dfe9cbab6e6abb5b4d79d14
FatuInsau <insaufaty@gmail.com>
2023-03-01 02:38:00 -0300

    ultimos retoques

--- app.js
+++ app.js
@@ -10,70 +10,75 @@ fetch('https://randomuser.me/api/')
     .then(data => {
         var info = data.results[0];
 
-        divPrincipal.innerHTML += `<div class="row align-items-start container">
-        <div class="col-md-4 col-lg-6">
-            <div class="principal">
-                <img src="${info.picture.large}" alt="foto de la persona" class="rounded-circle img-fluid">
-                <h3>Acerca de mí</h2>
-                <p id="contactos">Soy una persona trabajadora, con iniciativa y buena predisposición para ayudar.</p>
-            
-                <h3>Datos de contacto</h2>
-                <p><b>Celular:</b> ${info.cell}</p>
-                <p class="d-none d-lg-block"><b>Correo:</b> ${info.email}</p>
-                <p><b>Dirección:</b> ${info.location.street.name} ${info.location.street.number}, ${info.location.city}, ${info.location.state}, ${info.location.country}</p>
-                <br id="educados"/>
+        divPrincipal.innerHTML += `
+        <div class="row align-items-start container">
+            <div class="col-md-4 col-lg-6">
+                <div class="principal">
+                    <img src="${info.picture.large}" alt="foto de la persona" class="rounded-circle img-fluid">
+                    <div class="d-block d-md-none">
+                        <h1 class="display-2">${info.name.title + ' ' + info.name.first + ' ' + info.name.last}</h3>
+                        <h2 class="display-4 color-black" id="laboral">Lic. en Administración</h4>
+                        <hr/>
+                    </div>
+                    <h3>Acerca de mí</h2>
+                    <p id="contactos">Soy una persona trabajadora, con iniciativa y buena predisposición para ayudar.</p>
+                    <h3>Datos de contacto</h2>
+                    <p><b>Celular:</b> ${info.cell}</p>
+                    <p class="d-none d-lg-block"><b>Correo:</b> ${info.email}</p>
+                    <p><b>Dirección:</b> ${info.location.street.name} ${info.location.street.number}, ${info.location.city}, ${info.location.state}, ${info.location.country}</p>
+                    <br id="educados"/>
+                </div>
+                <div>
+                    <br/>
+                    <h3>Educación</h2>
+                    <p><b>Universidad Increíble</b></p>
+                    <ul>
+                        <li>Graduado con honores académicos</li>
+                        <li>Vicepresidente del Club de Matemática, 2012</li>
+                    </ul>
+                    <p> Lic. en Administración, 2018 </p>
+                </div>
             </div>
-            <div>
-            <br/>
-                <h3>Educación</h2>
-                <p><b>Universidad Increíble</b></p>
-                <ul>
-                    <li>Graduado con honores académicos</li>
-                    <li>Vicepresidente del Club de Matemática, 2012</li>
-                </ul>
-                <p> Lic. en Administración, 2018 </p>
+            <div class="col-md-8 col-lg-6">
+                <div class="d-none d-md-block">
+                    <h1 class="display-2">${info.name.title + ' ' + info.name.first + ' ' + info.name.last}</h3>
+                    <h2 class="display-4 color-black" id="laboral">Lic. en Administración</h4>
+                    <hr/>
+                </div>
+                <div>
+                    <h3>Experiencia Laboral</h2>
+                    <p><b>Asistente de Gerencia</b></p>
+                    <ul>
+                        <li>Asistencia administrativa integral a Gerencia.</li>
+                        <li>Seguimiento de Agenda. Revisión de Idoneidad de documentos y control de archivo. Creación de Presentaciones mensuales.</li>
+                    </ul>
+                    <p>Empresa Increíble, Ago 2019 - Presente</p>
+                    <hr>
+                    <p><b>Asistente Administrativo</b></p>
+                    <ul>
+                        <li>Recepción de Clientes. Manejo de Conmutador.</li>
+                        <li>Asistencia a mesa de entradas. Organización de libro de entrada y salida. Mantenimiento de archivo.</li>
+                        <li>Preparación de informes semanales.</li>
+                    </ul>
+                    <p>Empresa Increíble, Ene 2016 - Jul 2017</p>
+                    <hr>
+                    <p><b>Pasante Administrativo</b></p>
+                    <ul>
+                        <li>Recepción de Clientes. Manejo de Conmutador. </li>
+                        <li>Asistencia a mesa de entradas. Organización de libro de entrada y salida. Mantenimiento de archivo.</li>
+                        <li>Preparación de informes semanales.</li>
+                    </ul>
+                    <p>Empresa Increíble, Ene 2016 - Jul 2017</p>
+                    <hr>
+                    <p><b>Atención al Público</b></p>
+                    <ul>
+                        <li>Atención a Clientes personalmente.</li>
+                        <li>Mantenimiento y seguimiento de Stock y logística.</li>
+                        <li>Preparación de informes semanales.</li>
+                    </ul>
+                    <p>Empresa Increíble, Ene 2014 - Jul 2015</p>
+                </div>
             </div>
-        </div>
-        <div class="col-md-8 col-lg-6">
-            <div>
-                <h1 class="display-2">${info.name.title + ' ' + info.name.first + ' ' + info.name.last}</h3>
-                <h2 class="display-4 color-black" id="laboral">Lic. en Administración</h4>
-                <hr/>
-            </div>
-            <div>
-                <h3>Experiencia Laboral</h2>
-                <p><b>Asistente de Gerencia</b></p>
-                <ul>
-                    <li>Asistencia administrativa integral a Gerencia.</li>
-                    <li>Seguimiento de Agenda. Revisión de Idoneidad de documentos y control de archivo. Creación de Presentaciones mensuales.</li>
-                </ul>
-                <p>Empresa Increíble, Ago 2019 - Presente</p>
-                <hr>
-                <p><b>Asistente Administrativo</b></p>
-                <ul>
-                    <li>Recepción de Clientes. Manejo de Conmutador.</li>
-                    <li>Asistencia a mesa de entradas. Organización de libro de entrada y salida. Mantenimiento de archivo.</li>
-                    <li>Preparación de informes semanales.</li>
-                </ul>
-                <p>Empresa Increíble, Ene 2016 - Jul 2017</p>
-                <hr>
-                <p><b>Pasante Administrativo</b></p>
-                <ul>
-                    <li>Recepción de Clientes. Manejo de Conmutador. </li>
-                    <li>Asistencia a mesa de entradas. Organización de libro de entrada y salida. Mantenimiento de archivo.</li>
-                    <li>Preparación de informes semanales.</li>
-                </ul>
-                <p>Empresa Increíble, Ene 2016 - Jul 2017</p>
-                <hr>
-                <p><b>Atención al Público</b></p>
-                <ul>
-                    <li>Atención a Clientes personalmente.</li>
-                    <li>Mantenimiento y seguimiento de Stock y logística.</li>
-                    <li>Preparación de informes semanales.</li>
-                </ul>
-                <p>Empresa Increíble, Ene 2014 - Jul 2015</p>
-            </div>
-        </div>
-    </div> `
+        </div> `
     });
 
